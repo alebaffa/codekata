4f1f306610d8cb960bd2a7e145da4eee311c9a8a
Alessandro Baffa <alessandro.baffa@gmail.com>
2016-06-19 19:22:47 +0200

    red phase: add first test case up to 3

--- fizzbuzz/fizzbuzz_test.go
+++ fizzbuzz/fizzbuzz_test.go
@@ -1,2 +1,12 @@
 package fizzbuzz
 
+import "testing"
+
+func TestFizzBuzzTo3(t *testing.T)  {
+	expected := "12Fizz"
+	actual := FizzBuzz(3)
+
+	if expected != actual {
+		t.Error("Error! Expected %s", expected, " but received %s", actual)
+	}
+}
