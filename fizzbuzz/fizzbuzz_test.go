f4b46a3a89101ba8396417fd17818c523901d80e
Alessandro Baffa <alessandro.baffa@gmail.com>
2016-06-19 19:28:14 +0200

    red phase: add test FizzBuzz to 5.

--- fizzbuzz/fizzbuzz_test.go
+++ fizzbuzz/fizzbuzz_test.go
@@ -10,3 +10,12 @@ func TestFizzBuzzTo3(t *testing.T)  {
 		t.Error("Error! Expected %s", expected, " but received %s", actual)
 	}
 }
+
+func TestFizzBuzzTo5(t *testing.T)  {
+	expected := "12Fizz4Buzz"
+	actual := FizzBuzz(5)
+
+	if expected != actual {
+		t.Error("Error! Expected %s", expected, " but received %s", actual)
+	}
+}
