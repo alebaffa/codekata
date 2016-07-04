6204bb325742b9a0984d88feb4bcca7c38768c05
Alessandro Baffa <alessandro.baffa@gmail.com>
2016-07-04 22:58:10 +0200

    Roman Numerals kata - apply TPP(9): (statement -> recursion)

--- src/RomanNumbers.java
+++ src/RomanNumbers.java
@@ -6,13 +6,14 @@ public class RomanNumbers {
 	private static Map<Integer, String> dictionary = new HashMap<>();
 	static {
 		dictionary.put(1, "I");
-		dictionary.put(2, "II");
-		dictionary.put(3, "III");
 		dictionary.put(4, "IV");
 	};
 
 	public String convertToRoman(int number) {
 
-		return dictionary.get(number);
+		if(dictionary.containsKey(number))
+			return dictionary.get(number);
+
+		return dictionary.get(1) + convertToRoman(number - 1);
 	}
 }
